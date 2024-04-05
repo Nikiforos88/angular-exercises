409a85f4bf84f2f67e532055853cd420acb972ea
Nikiforos88 <nikiforosamg@gmail.com>
2024-04-05 17:48:33 +0300

    step 2 completed

--- angular-exercises/src/app/components/input/input.component.ts
+++ angular-exercises/src/app/components/input/input.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { EvenLengthWordsComponent } from '../even-length-words/even-length-words.component';
+import { OddLengthWordsComponent } from '../odd-length-words/odd-length-words.component';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [EvenLengthWordsComponent, OddLengthWordsComponent],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
-
+  value: string = '';
   userInput: string = '';
 
+  submit() {
+    if ( this.value.length % 2 === 0 ) {
+      console.log('odd')
+    } else {
+      console.log('even')
+    }
+    this.value = '';
+  }
+
   onUserInput(event: Event) {
-    const value = (event.target as HTMLInputElement).value;
-    this.userInput = value;
+    this.value= (event.target as HTMLInputElement).value;
+    this.userInput = this.value;
   }
 }
